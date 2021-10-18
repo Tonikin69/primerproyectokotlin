2f7bdd90387bbb10d623566ee7fac40d595fa660
Antonio <antoniotrotuman@gmail.com>
2021-10-18 13:32:12 +0200

    gg

--- src/main/kotlin/lamdas.kt
+++ src/main/kotlin/lamdas.kt
@@ -4,4 +4,11 @@ fun main(){
     decirhola()
     var deciralgo = {algo : String, otracosa : Int -> println("Digo $algo $otracosa")}
     deciralgo("boom!",69)
+
+    var sumar = {num1 : Int, num2 : Int -> num1+num2}
+    println(sumar(5,5))
+
+    var lambda ={letra : Char -> if (!letra.isDigit()) print(letra) }
+
+    "p1e4z8".forEach(lambda)
 }
