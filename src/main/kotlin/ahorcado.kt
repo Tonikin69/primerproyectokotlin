b3ec97c10771f3d896eb51262f071d9a97026ae1
Antonio <antoniotrotuman@gmail.com>
2021-10-18 12:33:24 +0200

    gg

--- src/main/kotlin/ahorcado.kt
+++ src/main/kotlin/ahorcado.kt
@@ -1,10 +1,33 @@
+import kotlin.random.Random
+
 fun main(){
-    var lista  = mutableListOf<Char>('a','h','o','r','c','a','d','o')
-    var lista2 = mutableListOf<Char>('_','_','_','_','_','_','_','_')
+    var lista  = mutableListOf<Char>()
     var letras = arrayListOf<Char>()
 
     var flag=false
+    var flaq2=false
+    var flaq3=false
     var cont=0
+    var continnt=0
+    var letrita=' '
+
+
+    println("introduce letra a letra tu palabra para el ahorcado e introduce x para no introducir mas")
+    while (flaq3==false){
+        letrita = readCharFromKeyboard()
+        if (letrita!='x'){
+            lista.add(letrita)
+        } else{
+            flaq3=true
+        }
+    }
+
+    val lista2 = MutableList(lista.size) {
+        '_'
+    }
+    println(lista)
+    println(lista2)
+
 
 
     while (flag==false){
@@ -23,21 +46,29 @@ fun main(){
                     lista2[it]=intento
                     cont++
                 }else{
+                    if (flaq2==false){
+                        continnt+=1
+                    }
+                    flaq2=true
 
                     letras.add(intento)
+
                 }
 
             }
-        if(cont==8){
+        if(cont==lista.size){
             flag=true
+            flaq2=false
         }
 
 
         println(lista2)
 
 
-    }
 
+
+    }
+    println("Has utilizado $continnt intentos")
 }
 
 fun readCharFromKeyboard(): Char {
