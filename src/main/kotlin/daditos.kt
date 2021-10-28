81fdb4a8cc7be8e788d58048bb68243c63d28738
Antonio <antoniotrotuman@gmail.com>
2021-10-28 12:58:21 +0200

    gg

---
+++ src/main/kotlin/daditos.kt
@@ -0,0 +1,67 @@
+import kotlin.random.Random
+val listresultado = mutableListOf<Participante>()
+fun main() {
+
+
+    repeat(100){
+        var juga = Participante(it)
+       listresultado.add(juga)
+    }
+    listresultado.sortBy { it.conter }
+
+
+    println(mostrarResultado())
+
+
+}
+fun mostrarResultado(): String {
+    var resultado=""
+    for (i in listresultado.reversed()){
+        resultado+="Jugador "+i.id+" ha sacado "+i.conter+ " puntos\n"
+    }
+    return resultado
+}
+
+class Dado (){
+     private var numMin = 1
+     private var numMax = 6
+
+    constructor(numMin :Int, numMax :Int) : this(){
+        this.numMin=numMin
+        this.numMax=numMax
+    }
+
+    fun tiradaUnica (): Int {
+        return Random.nextInt(numMin, numMax)
+    }
+
+}
+
+class Participante (id :Int) {
+    var id=id
+    var conter = proceso()+proceso2()+proceso3()
+
+
+    fun proceso() :Int{
+        var tirada = Dado()
+        repeat(100){
+            conter+=tirada.tiradaUnica()
+        }
+        return conter
+    }
+    fun proceso2() :Int{
+        var tirada2 = Dado(1,3)
+        repeat(100){
+            conter+=tirada2.tiradaUnica()
+        }
+        return conter
+    }
+    fun proceso3() :Int {
+        var tirada3 = Dado(4, 6)
+        repeat(100) {
+            conter += tirada3.tiradaUnica()
+        }
+        return conter
+    }
+
+}
